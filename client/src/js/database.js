3f642f9e2cdf7bdf100c18790ec3a4f287194d32
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-13 17:03:43 -0400

    fixed logic to properly send and retrieve data from indexeddb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,22 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ id: 1, value: content});
+  const result = await request;
+  console.log('Data saved to database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result.value;
+}
 
 initdb();
